245e8e7c75ca5f4369d7d7ad87dd00863edfed27
Timothée Fleury <timothee@fromsmash.com>
2017-07-07 15:11:08 +0200

    Add 3 commands to init projects

--- templates/lambda/index.js
+++ templates/lambda/index.js
@@ -1,8 +1,6 @@
 var smash = require("smash-js-serverless");
 
 exports.handler = (event, context, callback) => {
-    smash.registerRequestMiddleware();
-    smash.registerResponseMiddleware();
     smash.boot(true);
     smash.handleRequest(event, callback);
 };
