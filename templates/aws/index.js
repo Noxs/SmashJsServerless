27168fad4a391cb2c08b38fe0b8c5f0df9db89bd
Timothée Fleury <timothee@fromsmash.com>
2017-09-06 19:27:38 +0200

    Fix lambda template index.js

--- templates/aws/index.js
+++ templates/aws/index.js
@@ -1,6 +1,6 @@
 var smash = require("smash-js-serverless");
 
 exports.handler = (event, context, callback) => {
-    smash.boot(true);
+    smash.boot(process.env, true);
     smash.handleRequest(event, callback);
 };
