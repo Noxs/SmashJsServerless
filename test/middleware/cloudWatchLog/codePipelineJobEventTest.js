d9b840b7d82637098785de6bd3d9d4cff7a18451
Timothée Fleury <timothee@fromsmash.com>
2018-02-07 11:25:29 +0100

    Add cloudwatchlog middleware

---
+++ test/middleware/cloudWatchLog/codePipelineJobEventTest.js
@@ -0,0 +1,11 @@
+const chai = require('chai');
+const assert = chai.assert;
+const expect = chai.expect;
+const should = chai.should();
+const CodePipelineJobEvent = require('../../../lib/middleware/CloudWatchLog/CloudWatchLog.js');
+
+describe('CloudWatchLog', function () {
+    it('Test CloudWatchLog instance', function () {
+        //TODO
+    });
+});
