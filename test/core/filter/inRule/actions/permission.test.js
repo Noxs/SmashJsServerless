6190325e7a5a9c469df3f783d25791939a88e317
Timothée Fleury <timothee@fromsmash.com>
2020-07-15 15:12:39 +0200

    Improve permission system

--- test/core/filter/inRule/actions/permission.test.js
+++ test/core/filter/inRule/actions/permission.test.js
@@ -21,9 +21,7 @@ describe('Permission', () => {
 	});
 
 	it('Test validate case #1', () => {
-		const mockedFunction = jest.fn(() => {
-			return true;
-		});
+		const mockedFunction = jest.fn(() => true);
 		expect(permission.validate({
 			current: {
 				name: "permission",
@@ -41,51 +39,63 @@ describe('Permission', () => {
 	});
 
 	it('Test validate case #2', () => {
-		expect(() => {
-			return permission.validate({
-				current: {
-					name: "permission",
-					value: 1,
+		expect(() => permission.validate({
+			current: {
+				name: "permission",
+				value: 1,
+			},
+			rule: { permission: 1 },
+			parents: [
+				{
+					name: "none",
+					value: { permission: 1 },
 				},
-				rule: { permission: 1 },
-				parents: [
-					{
-						name: "none",
-						value: { permission: 1 },
-					},
-				],
-				ruleConfig: { version: "01-2019", action: "MyFooBarAction", type: "inRule" },
-			});
-		}).toThrow();
+			],
+			ruleConfig: { version: "01-2019", action: "MyFooBarAction", type: "inRule" },
+		})).toThrow();
 	});
 
 	it('Test validate case #3', () => {
-		expect(() => {
-			return permission.validate({
-				current: {
-					name: "permission",
-					value: "foobar",
+		expect(() => permission.validate({
+			current: {
+				name: "permission",
+				value: "foobar",
+			},
+			rule: { permission: "foobar" },
+			parents: [
+				{
+					name: "none",
+					value: { permission: "foobar" },
 				},
-				rule: { permission: "foobar" },
-				parents: [
-					{
-						name: "none",
-						value: { permission: "foobar" },
-					},
-				],
-				ruleConfig: { version: "01-2019", action: "MyFooBarAction", type: "inRule" },
-			});
-		}).toThrow();
+			],
+			ruleConfig: { version: "01-2019", action: "MyFooBarAction", type: "inRule" },
+		})).toThrow();
+	});
+
+	it('Test validate case #4', () => {
+		const mockedFunction = jest.fn(() => true);
+		expect(permission.validate({
+			current: {
+				name: "permission",
+				value: [mockedFunction, mockedFunction],
+			},
+			rule: { permission: [mockedFunction, mockedFunction] },
+			parents: [
+				{
+					name: "none",
+					value: { permission: [mockedFunction, mockedFunction] },
+				},
+			],
+			ruleConfig: { version: "01-2019", action: "MyFooBarAction", type: "inRule" },
+		})).toBe(true);
 	});
 
 	it('Test execute case #1', async () => {
 		const initialData = { parameters: {}, body: {} };
 		const current = { name: "myVar", value: undefined };
-		const mockFunction = jest.fn(() => {
-			return new Promise((resolve, reject) => {
-				resolve("newValue");
-			});
-		});
+		const mockFunction = jest.fn(() => new Promise((resolve, reject) => {
+			resolve("newValue");
+		}));
 		await expect(permission.execute({
 			rule: {
 				current: {
@@ -112,11 +122,9 @@ describe('Permission', () => {
 	it('Test execute case #2', async () => {
 		const initialData = { parameters: {}, body: {} };
 		const current = { name: "myVar", value: undefined };
-		const mockFunction = jest.fn(() => {
-			return new Promise((resolve, reject) => {
-				reject(new Error());
-			});
-		});
+		const mockFunction = jest.fn(() => new Promise((resolve, reject) => {
+			reject(new Error());
+		}));
 		await expect(permission.execute({
 			rule: {
 				current: {
@@ -138,6 +146,35 @@ describe('Permission', () => {
 				parents: [{ name: "none", value: { parameters: {}, body: { myVar: undefined } } }, { name: "body", value: { myVar: undefined } }],
 			},
 		})).rejects.toThrow();
+	});
+
+	it('Test execute case #1', async () => {
+		const initialData = { parameters: {}, body: {} };
+		const current = { name: "myVar", value: undefined };
+		const mockFunction = jest.fn(() => new Promise((resolve, reject) => {
+			resolve("newValue");
+		}));
+		await expect(permission.execute({
+			rule: {
+				current: {
+					name: "permission",
+					value: [mockFunction, mockFunction],
+				},
+				initalRule: { permission: [mockFunction, mockFunction] },
+				parents: [
+					{
+						name: "none",
+						value: { permission: [mockFunction, mockFunction] },
+					},
+				],
+				ruleConfig: { version: "01-2019", action: "MyFooBarAction", type: "inRule" },
+			},
+			data: {
+				current,
+				initialData,
+				parents: [{ name: "none", value: { parameters: {}, body: { myVar: undefined } } }, { name: "body", value: { myVar: undefined } }],
+			},
+		})).resolves.toBe(true);
 	});
 });
 
